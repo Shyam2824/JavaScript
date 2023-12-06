14f3d4ba3cd9e1eaf6aea1266369cf9da50f2f4e
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-06 11:54:41 +0530

    to check operator output

---
+++ 03_conversionalOperator.js
@@ -0,0 +1,17 @@
+//let value = "55as"
+let value = null // if null then zero output come 
+
+console.log(typeof(value));
+console.log(typeof value);
+let valueInNumber = Number(value)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // output id NAN if that should be not complete number
+// "33" => 33
+// undefine => undefine
+// "336sfs" => NaN not a number
+// true=>1 / false=> 0
+
+let login = 1
+
+let booleanlogined = Boolean(login)
+console.log(booleanlogined);
