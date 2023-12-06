3ef62ae46b57946085656e40f57fdb86cb1f7dc2
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-06 11:39:42 +0530

    javascript code and notes

---
+++ 01_first.js
@@ -0,0 +1,15 @@
+//console.log("Shyam")
+const accountId = 14547
+let accountEmail = "shyam@gmail.com"
+let accountPassword = "266565"
+accountId = "gaya"
+accountUser;
+/* 
+"Const" cant be changed 
+prefer not use "var" because of issue in block scope and functional scope
+*/
+accountEmail = "sundar@gmail.com"
+    //accountId = 1455
+accountPassword = "8684"
+accountCity = "BodhGaya"
+console.table([accountId, accountEmail, accountPassword, accountId, accountUser])
---
+++ 02_data_type.js
@@ -0,0 +1,20 @@
+/*
+permeative data type
+number=> 2 to  53
+bigint=> used to large number of data 
+string=> "" or '' both of the used
+boolean => true / false
+null => standalone value
+undefine=> 
+symbol=> unique
+
+object
+*/
+console.log(typeof "shyam");
+console.log(typeof null); // output should be "object " 
+console.log(typeof undefined); // output should be "undefined"
+let age = 19
+let goback = true
+let name = "shyam"
+    //console.table([age, goback, name]);
+console.log(typeof age);
