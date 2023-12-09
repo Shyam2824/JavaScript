7b31fccb09a7e02c61e4b56a7a78d8f865cfcc79
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-09 19:15:57 +0530

    number and maths

---
+++ 06_num_maths.js
@@ -0,0 +1,15 @@
+const firstNumber = 5000
+console.log(firstNumber);
+
+const account = new Number(5245) // if number is generated that converted in many format
+console.log(account);
+console.log(account.toString()); // to String
+console.log(account.toString().length); // for length
+console.log(account.toFixed(3)); // give the decimal value .000
+
+
+const otherNumber = 12500000
+    //console.log(otherNumber.toPrecision(4)); //to locate and range of the number
+console.log(otherNumber.toLocaleString());
+
+//-------------------Maths------------------------
