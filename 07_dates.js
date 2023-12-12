9dcdaff4e38aa326643b26fbef1e658845a129b8
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-12 12:26:06 +0530

    date

---
+++ 07_dates.js
@@ -0,0 +1,7 @@
+// ---------Dates-----------------
+
+let myDate = new Date()
+console.log(myDate); //2023-12-12T06:46:33.888Z output
+console.log(myDate.toString()); //Tue Dec 12 2023 12:18:12 GMT+0530 (India Standard Time)
+console.log(myDate.toDateString()); // Tue Dec 12 2023
+console.log(myDate.toTimeString()); //12:18:12 GMT+0530 (India Standard Time)
