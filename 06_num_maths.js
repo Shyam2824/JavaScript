aad8cb17a087c5a299cfbebf370373fd90e2e7ef
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-12 12:11:06 +0530

    math values

--- 06_num_maths.js
+++ 06_num_maths.js
@@ -14,8 +14,12 @@ console.log(otherNumber.toLocaleString());
 
 //-------------------Maths------------------------
 
-console.log(Math.random());
+console.log(Math.random()); // given value under 0 & 1
 console.log((Math.random() * 10) + 1); // any random number
 console.log(Math.floor(Math.random() * 10) + 1); // round of in lower value
 console.log(Math.ceil(Math.random() * 10) + 1); // round of in higher value
 console.log(Math.max());
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
