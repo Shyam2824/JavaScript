deac23f4e1cb34439f50da1cc063174df238ae26
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-13 18:07:28 +0530

    array part 2

---
+++ 09_array.js
@@ -0,0 +1,8 @@
+const c_name = ["hcl", "lenovo", "mi", "bajaj"]
+const m_name = ["k2", "everest", "buddha"]
+
+c_name.push(m_name)
+    //console.log(c_name);
+
+const my_name = c_name.concat(m_name)
+console.log(my_name);
