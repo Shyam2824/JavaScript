59a661084330da98352d1be7f51acc3c4fb19cd8
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-15 17:34:05 +0530

    function

--- 10_object.js
+++ 10_object.js
@@ -5,18 +5,28 @@
 const mySy = Symbol("key1")
 
 const object1 = {
-    name: "Shyam  Sundar",
-    email: "shyam@google.com",
-    age: 21,
-    [mySy]: "key1",
-    location: "gaya",
-}
-console.log(object1.email);
-console.log(object1["email"]);
-console.log(object1[mySy]);
-console.log(object1.location);
+        name: "Shyam  Sundar",
+        email: "shyam@google.com",
+        age: 21,
+        [mySy]: "key1",
+        location: "gaya",
+    }
+    // console.log(object1.email);
+    // console.log(object1["email"]);
+    // console.log(object1[mySy]);
+    // console.log(object1.location);
 
 object1.name = "Shyam Sundar Singh"
-Object.freeze(object1) // that not allowed to change any things
-object1.name = "sss"
-console.log(object1);
+    //Object.freeze(object1) // that not allowed to change any things
+    //object1.name = "sss"
+    //console.log(object1);
+
+// ************ Function *************
+object1.greeting = function() {
+    console.log(" Hello JS user ");
+}
+object1.greetingTwo = function() {
+    console.log(` Hello JS user , ${this.name}`); // that should be refered the name "this should be used to the "reference ""
+}
+console.log(object1.greeting());
+console.log(object1.greetingTwo());
