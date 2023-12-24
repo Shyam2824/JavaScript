9cf58363438123aa8a610695f7585a34be6094ca
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-24 09:24:26 +0530

    object singletone

---
+++ 11_objects.js
@@ -0,0 +1,20 @@
+// Single tone object
+
+const timePass = {}
+timePass.id = "Sjyg56865"
+timePass.name = "Golu Singh"
+timePass.location = "gaya"
+timePass.isLogIn = false
+
+//console.log(timePass);
+
+const remainingTime = {
+    email: "remainingtime@gmail.com",
+    fullName: {
+        userNmae: {
+            firstName: " Golu",
+            lastName: "Singh",
+        }
+    }
+}
+console.log(remainingTime.fullName.userNmae.firstName);
