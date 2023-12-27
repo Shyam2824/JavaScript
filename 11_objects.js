52820f06cf168309b8e11fc832ae1bd6074735dd
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-27 11:15:30 +0530

    json & Object

--- 11_objects.js
+++ 11_objects.js
@@ -24,5 +24,25 @@ const obj2 = { 3: "a", 4: "b" }
 const obj4 = { 5: "a", 6: "e" }
     //const obj3 = Object.assign({}, obj1, obj2, obj4);
 
-const obj3 = {...obj1, ...obj2, ...obj4 }
-console.log(obj3);
+const obj3 = {...obj1, ...obj2, ...obj4 } // expand function
+    //console.log(obj3);
+    // console.log(Object.values(timePass));
+    // console.log(Object.entries(timePass));
+
+const course = {
+    courseNme: "Code in js",
+    coursePrise: 258,
+    courseInstructor: "Golu Singh"
+}
+
+//console.log(course.courseInstructor);
+
+const { courseInstructor: instructor } = course
+console.log(instructor);
+
+
+// json formate
+// {
+//     "name" : "dhydvhdvy",
+//     "course" : "java Script",
+// }
