92fb7084321beff7d02ca93c4b87439cf4754c89
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-03 21:34:08 +0530

    nisted scope

--- 13_scope.js
+++ 13_scope.js
@@ -6,9 +6,41 @@ let a = 30
 if (true) {
     let a = 20
     const b = 50
-    console.log("Inner: " + a);
+        // console.log("Inner: " + a);
 }
 
+//console.log(a);
 
+//+++++ Nested Scope++++++++++
+function one() {
+    const userNmae = "Shyam"
 
-console.log(a);
+    function two() {
+        const Work = "Study"
+        console.log(userNmae);
+    }
+    //console.log(work); // that should not be defined because that should not be work outside the function key
+    two()
+}
+
+one()
+
+// now in if else conditions
+if (true) {
+    const userNmae = "Shyam "
+
+    if (userNmae === "Shyam ") {
+        const webWork = "portfolio"
+        console.log(userNmae + webWork);
+    }
+    // console.log(webWork); // that should not to be work because that should out side the scope
+}
+//console.log(userNmae);// that should also out side the scope
+
+// +++++++++ Interesting +++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num) {
+    return +1
+}
