d3860bab19fc0734bbcb80a40f02da3147d717d2
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-06 11:06:18 +0530

    if else condition

---
+++ 17_control.js
@@ -0,0 +1,13 @@
+// // if 
+
+// const isUserLogIn = true
+// const temperature = 46
+// if (temperature === 46) {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("temperature is less than 50");
+
+// // <,>,>=,<=,==,===,!==
+
+const score = 258
+if (score == 500) console.log("test");
