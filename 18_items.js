4e44880264ca85a02d93c904da26519370b06398
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-04-21 20:02:14 +0530

    objet type and practice question

---
+++ 18_items.js
@@ -0,0 +1,29 @@
+const items = {
+    "Shyam": 45,
+    "ram": undefined,
+    "sohan": true
+}
+
+console.log(items["ram"])
+
+
+//  practice se ch 1  
+// Q1.  add two variable , one is string and other are numerical
+
+let a = "computer"
+let b = 5
+
+console.log(a + b)
+
+// Q2. datatype
+console.log(typeof(a + b))
+
+//  Q3. create object
+
+const f = {
+    name: "shyam",
+    class: 12,
+    isCompleted: true
+}
+f['friend'] = "Adarsh"
+console.log(f)
